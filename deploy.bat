087d3f53dd30dcc21bbc007a71f0d5b6ba647b42
TheFloatingBrain <TheFloatingBrain@gmail.com>
2020-02-25 22:14:01 -0500

    $1

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 git ls-files -d > deleted_files.txt
 git add *
-diff --name-only --diff-filter=A HEAD > newly_created_files.txt
+git diff --name-only --diff-filter=A HEAD > newly_created_files.txt
 git ls-files -m > modified_files.txt
 git commit -m $1
 git push origin master
